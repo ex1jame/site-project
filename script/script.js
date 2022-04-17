9d8bb2057ca0edd4cfea970caed2d77085b9c2c9
ex1jame <81546043+ex1jame@users.noreply.github.com>
2022-04-17 17:35:25 +0600

    customize hero

---
+++ script/script.js
@@ -0,0 +1,11 @@
+let switchMode = document.getElementById("switchMode")
+
+switchMode.onclick = function () {
+    let theme = document.getElementById("theme")
+
+    if (theme.getAttribute("href") == "style/light-mode.css"){
+        theme.href = "style/dark-mode.css"
+    }else {
+        theme.href = "style/light-mode.css"
+    }
+}
